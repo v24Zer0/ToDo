3ebc0a11022c6a5a9020e41b1af59237daa3cc7b
v24Zer0 <vinbesse@gmail.com>
2022-03-26 17:16:32 +0200

    Add new logger for database

--- item-service/database/database.go
+++ item-service/database/database.go
@@ -2,10 +2,13 @@ package database
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"time"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func NewDatabase() (*gorm.DB, error) {
@@ -16,7 +19,17 @@ func NewDatabase() (*gorm.DB, error) {
 
 	dbPath := fmt.Sprintf("%s:%s@tcp%s/%s", dbUser, dbPass, dbURL, dbName)
 
-	db, err := gorm.Open(mysql.Open(dbPath), &gorm.Config{})
+	newLogger := logger.New(log.New(os.Stdout, "[gorm]", log.LstdFlags), logger.Config{
+		SlowThreshold:             time.Second,
+		Colorful:                  false,
+		IgnoreRecordNotFoundError: true,
+		LogLevel:                  logger.Silent,
+	})
+
+	db, err := gorm.Open(mysql.Open(dbPath), &gorm.Config{
+		Logger: newLogger,
+	})
+
 	if err != nil {
 		return nil, err
 	}
