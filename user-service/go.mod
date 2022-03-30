82d1784b8d75ba5bb44514665a6e3f35da6af369
v24Zer0 <vinbesse@gmail.com>
2022-03-30 13:29:32 +0200

    http server init
    Created http server with graceful shutdown, mux router and environment variable load

---
+++ user-service/go.mod
@@ -0,0 +1,8 @@
+module github.com/v24Zer0/ToDO/user-service
+
+go 1.17
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
--- user-service/main.go
+++ user-service/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Error loading environment variables")
+	}
+
+	router := mux.NewRouter()
+
+	server := http.Server{
+		Addr:    ":9091",
+		Handler: router,
+	}
+
+	// start http server
+	go func() {
+		log.Println("Server starting on port 9091")
+
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Println("Server shutdown")
+		}
+	}()
+
+	// trap sigterm or interupt
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+	// Block until a signal is received.
+	sig := <-c
+	log.Println("Got signal:", sig)
+
+	// gracefully shutdown the server, waiting max 30 seconds for current operations to complete
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	server.Shutdown(ctx)
+	os.Clearenv()
+}
