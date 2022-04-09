76a355092798e069cb83b8cfba6a1df7ea7c435e
v24Zer0 <vinbesse@gmail.com>
2022-04-09 18:43:30 +0200

    user service auth impl

--- user-service/authentication/authentication.go
+++ user-service/authentication/authentication.go
@@ -1,5 +1,65 @@
 package authentication
 
-func GetToken(id string) string {
-	return ""
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/v24Zer0/ToDo/user-service/models"
+)
+
+type AuthToken struct {
+	Token string
+	Error error
+}
+
+func GetToken(c chan AuthToken, id string) {
+	authURL := os.Getenv("AuthService")
+	url := fmt.Sprintf("%s/%s", authURL, id)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		c <- AuthToken{Error: err}
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		c <- AuthToken{Error: errors.New("error fetching token")}
+		return
+	}
+
+	var userToken models.UserToken
+	userToken.Decode(resp.Body)
+
+	c <- AuthToken{Token: userToken.Token}
+}
+
+func CreateToken(c chan bool, id string) {
+	authURL := os.Getenv("AuthService")
+
+	var payload bytes.Buffer
+	userToken := models.UserToken{UserID: id}
+
+	err := userToken.Encode(&payload)
+	if err != nil {
+		c <- false
+		return
+	}
+
+	resp, err := http.Post(authURL, "application/json", &payload)
+	if err != nil {
+		c <- false
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 201 {
+		c <- false
+		return
+	}
+
+	c <- true
 }
---
+++ user-service/models/token.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"encoding/json"
+	"io"
+)
+
+type UserToken struct {
+	UserID string `json:"user_id"`
+	Token  string `json:"token"`
+}
+
+func (t *UserToken) Decode(r io.Reader) error {
+	return json.NewDecoder(r).Decode(t)
+}
+
+func (t *UserToken) Encode(w io.Writer) error {
+	return json.NewEncoder(w).Encode(t)
+}
