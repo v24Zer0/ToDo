68c0c8f52e013ef96ff77d46607f38a700fb4ca4
v24Zer0 <vinbesse@gmail.com>
2022-05-12 10:57:12 +0200

    Creation of pages for navigation

---
+++ frontend/ToDo/src/pages/itemPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ItemPage = () => {
+    return (
+        <View>
+            <Text>
+                Item Page
+            </Text>
+        </View>
+    );
+}
+
+export default ItemPage;
---
+++ frontend/ToDo/src/pages/listPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const ListPage = () => {
+    return (
+        <View>
+            <Text>
+                List Page
+            </Text>
+        </View>
+    );
+}
+
+export default ListPage;
