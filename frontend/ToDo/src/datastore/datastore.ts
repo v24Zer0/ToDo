9d69db619d6e1ae57bb52538c3375003c67afe1f
v24Zer0 <vinbesse@gmail.com>
2022-05-17 13:33:20 +0200

    Added react-native-async-storage

---
+++ frontend/ToDo/src/datastore/datastore.ts
@@ -0,0 +1,26 @@
+/* 
+    Datastore for login and auth data implemented using react-native-async-storage 
+*/
+
+const storeAuthToken = (token: string) => {
+
+}
+
+const getAuthToken = () => {
+    return "";
+}
+
+const storeUserInfo = (userID: string, username: string, loggedIn: boolean) => {
+
+}
+
+const getUserInfo = () => {
+    return "";
+}
+
+export default {
+    storeAuthToken,
+    getAuthToken,
+    storeUserInfo,
+    getUserInfo
+};
